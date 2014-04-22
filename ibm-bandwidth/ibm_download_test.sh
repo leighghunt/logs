facfa9d4f162ac7d145c0e0cb0f461f754e1e28e
Leigh Hunt <leigh.hunt@splicegroup.co.nz>
2014-04-22 20:39:00 +1200

    ibm logs

---
+++ ibm-bandwidth/ibm_download_test.sh
@@ -0,0 +1,8 @@
+#wget planitroad.bureauco.com/UAT/Temp_Image/download_test_small.png -o ibm_download.log
+#wget planitroad.bureauco.com/UAT/Temp_Image/download_test_small.png -a ~/ibm_download_small.log -O ~/ibm_download_test_small.png
+
+#wget 203.167.168.139/UAT/Temp_Image/download_test_small.png -a ~/ibm_download_small.log -O ~/ibm_download_test_small.png
+wget planitroad.bureauco.com/UAT/Temp_Image/download_test_small.png -a ~/ibm_download_small.log -O ~/ibm_download_test_small.png
+#wget s1-bureauco.cloud.eaglegis.co.nz/UAT/Temp_Image/download_test_small.png -a ~/ibm_download_small.s1.log -O ~/ibm_download_test_small.s1.png
+
+#wget planitroad.bureauco.com/UAT/Temp_Image/download_test_tiny.png -a ~/ibm_download_tiny.log -O ~/ibm_download_test.png
---
+++ ibm-bandwidth/ibm_download_test_tiny.sh
@@ -0,0 +1,3 @@
+#wget planitroad.bureauco.com/UAT/Temp_Image/download_test_small.png -o ibm_download.log
+#wget planitroad.bureauco.com/UAT/Temp_Image/download_test_tiny.png -a ~/ibm_download_tiny.log -O ~/ibm_download_test.png
+wget 203.167.168.139/UAT/Temp_Image/download_test_tiny.png -a ~/ibm_download_tiny.log -O ~/ibm_download_test.png
